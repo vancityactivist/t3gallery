8b77c9a6e5222df22226284cc0bb8d63927f9dfa
Adam Glover <adamglover84@gmail.com>
2024-05-30 20:35:33 -0700

    break queries into seperate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 import Link from "next/link";
 import { db } from "~/server/db";
-
 import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
-
+import {getMyImages} from "~/server/queries";
 export const dynamic = "force-dynamic";
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc } ) => desc(model.id)
-  });
+
+  const images = await getMyImages();
   return (
     <div className="flex flex-wrap gap-4">
-        
         {images.map((image) => (
           <div key={image.id} className="w-48">
                 <img src={image.url} alt="Image" />
