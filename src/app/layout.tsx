d2c4dbaf3b6e63b62396d962c355d808a4706455
Adam Glover <adamglover84@gmail.com>
2024-06-01 00:48:55 -0700

    added shadcn toast - not implemented yet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
            */
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-      <body className={'font-sans ${inter.variable}'}>
+      <body className={'font-sans ${inter.variable}, dark'}>
       <div className="h-screen grid grid-rows-[auto,1fr]">
         <TopNav />
         <main className="overflow-y-scroll">{children}</main>
