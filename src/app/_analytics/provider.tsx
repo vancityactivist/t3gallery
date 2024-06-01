9bc26c0e45c210797974be7271feefc4891c83f4
Adam Glover <adamglover84@gmail.com>
2024-06-01 16:28:37 -0700

    added PostHog to App Router

--- src/app/_analytics/provider.tsx
+++ src/app/_analytics/provider.tsx
@@ -4,10 +4,11 @@ import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 import { useEffect } from 'react';
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  })
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
+  });
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
   return (
