567d5096999985990a4a6666d90a8779f5643fec
Adam Glover <adamglover84@gmail.com>
2024-05-31 23:55:41 -0700

    finished routing and most of image page. still some scaling issues

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -20,7 +20,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
 
   return createPortal(
     
-      <dialog ref={dialogRef} className="w-screen h-screen bg-zinc-900/50 text-white" onClose={onDismiss}>
+      <dialog ref={dialogRef} className="w-screen h-screen bg-black/90 text-white" onClose={onDismiss}>
         {children}
         {/* <button onClick={onDismiss} className="close-button" /> */}
       </dialog>
--- src/components/full-page-image.tsx
+++ src/components/full-page-image.tsx
@@ -1,17 +1,30 @@
 import { getImage } from "~/server/queries";
 import { Modal } from "./modal";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export default async function FullPageImageView(props: { id: number }) {
   
   const image = await getImage(props.id);
+
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
+
   return (
     <div className="flex w-full h-full min-w-0">
       <div className="flex-shrink flex justify-center items-center">
     <img src={image.url} className="flex-shrink object-contain " />
     </div>
-    <div className="w-48 flex flex-col flex-shrink-0 border-l">
-      <div className="text-xl font-bold">{image.name}</div>
-    </div>
+    <div className="w-48 flex flex-col flex-shrink-0 gap-2 border-l">
+      <div className="text-lg border-b text-center">{image.name}</div>
+
+        <div className="flex flex-col p-2">
+          <span>Uploaded by </span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created on </span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
+      </div>
     </div>
   );
 }
