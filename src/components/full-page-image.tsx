3ef4b1a75261017027b25693e0c9ec4d752f4292
Adam Glover <adamglover84@gmail.com>
2024-05-31 23:26:06 -0700

    some image work

--- src/components/full-page-image.tsx
+++ src/components/full-page-image.tsx
@@ -4,4 +4,14 @@ import { Modal } from "./modal";
 export default async function FullPageImageView(props: { id: number }) {
   
   const image = await getImage(props.id);
-  return <img src={image.url} className="w-96" />;}
+  return (
+    <div className="flex w-full h-full min-w-0">
+      <div className="flex-shrink flex justify-center items-center">
+    <img src={image.url} className="flex-shrink object-contain " />
+    </div>
+    <div className="w-48 flex flex-col flex-shrink-0 border-l">
+      <div className="text-xl font-bold">{image.name}</div>
+    </div>
+    </div>
+  );
+}
