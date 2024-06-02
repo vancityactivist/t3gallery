7e22bc53a4555638d022c0cf867076014afbd813
Adam Glover <adamglover84@gmail.com>
2024-06-01 17:10:35 -0700

    delete button

--- src/components/full-page-image.tsx
+++ src/components/full-page-image.tsx
@@ -1,9 +1,11 @@
-import { getImage } from "~/server/queries";
+import { deleteImage, getImage } from "~/server/queries";
 import { Modal } from "./modal";
 import { clerkClient } from "@clerk/nextjs/server";
+import { Button } from "./ui/button";
+
 
 export default async function FullPageImageView(props: { id: number }) {
-  
+  const idAsNumber = Number(props.id);
   const image = await getImage(props.id);
 
   const uploaderInfo = await clerkClient.users.getUser(image.userId);
@@ -16,13 +18,21 @@ export default async function FullPageImageView(props: { id: number }) {
     <div className="w-48 flex flex-col flex-shrink-0 gap-2 border-l">
       <div className="text-lg border-b text-center">{image.name}</div>
 
-        <div className="flex flex-col p-2">
+        <div className="p-2">
           <span>Uploaded by </span>
           <span>{uploaderInfo.fullName}</span>
         </div>
-        <div className="flex flex-col p-2">
+        <div className="p-2">
           <span>Created on </span>
           <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
+        <div className="p-2">
+          <form action={async() => {
+            "use server";
+            await deleteImage(idAsNumber);
+          }}>
+          <Button variant="destructive" >Delete</Button>
+          </form>
         </div>
       </div>
     </div>
